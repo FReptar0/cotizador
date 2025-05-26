3119f81c6c17e2d26d33826d2f054ff2cc5259b3
yahirdev13 <yahir.dev13@gmail.com>
2025-05-26 10:38:47 -0600

    Add ElevenLabs Convai widget script and element to _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,8 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* ElevenLabs Convai widget script */}
+        <script
+          src="https://elevenlabs.io/convai-widget/index.js"
+          async
+          type="text/javascript"
+        ></script>
+      </Head>
       <body>
+        {/* ElevenLabs Convai widget element */}
+        <elevenlabs-convai agent-id="agent_01jw1xmts8em4rgb4gwsjr35an"></elevenlabs-convai>
         <Main />
         <NextScript />
       </body>
